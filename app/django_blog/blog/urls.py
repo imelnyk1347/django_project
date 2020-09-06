6f7f8147c054069d3ed9454a86adc05ccaa4d82e
imelnyk1347 <imelnyk1347@gmail.com>
2020-09-06 19:55:25 +0300

    Create template for views(TagDetail,PostDetail)-mixin, add default error 404

--- app/django_blog/blog/urls.py
+++ app/django_blog/blog/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from .views import posts_list, post_detail, tags_list, tag_detail
+from .views import posts_list, PostDetail, tags_list, TagDetail
 
 
 urlpatterns = [
     path('', posts_list, name='posts_list_url'),
-    path('post/<str:slug>/', post_detail, name='post_detail_url'),
+    path('post/<str:slug>/', PostDetail.as_view(), name='post_detail_url'),
     path('tags/', tags_list, name='tags_detail_url'),
-    path('tags/<str:slug>', tag_detail, name='tag_detail_list_url')
+    path('tags/<str:slug>', TagDetail.as_view(), name='tag_detail_list_url')
 ]
