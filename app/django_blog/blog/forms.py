2728da8c7264fb114e33e1bf09b11abe2a300eec
imelnyk1347 <imelnyk1347@gmail.com>
2020-09-20 23:35:22 +0300

    Add page for create new post

--- app/django_blog/blog/forms.py
+++ app/django_blog/blog/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.core.exceptions import ValidationError
 
-from .models import Tag
+from .models import Tag, Post
 
 
 class TagForm(forms.ModelForm):
@@ -24,12 +24,33 @@ class TagForm(forms.ModelForm):
         new_slug = self.cleaned_data['slug'].lower()
 
         if new_slug == 'create':
-            raise ValidationError('Slug may not be "create"')
+            raise ValidationError('Slug may not be "create".')
         if Tag.objects.filter(slug__iexact=new_slug).count():
             raise ValidationError(f"Slug mast be unique. Sorry, slug '{new_slug}' now we have. Try another slug.")
         return new_slug
 
-    def save(self):
-        new_tag = Tag.objects.create(title=self.cleaned_data['title'],
-                                     slug=self.cleaned_data['slug'])
-        return new_tag
+    # def save(self):
+    #     new_tag = Tag.objects.create(title=self.cleaned_data['title'],
+    #                                  slug=self.cleaned_data['slug'])
+    #     return new_tag
+
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        fields = ['title', 'slug', 'body', 'tags']
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'slug': forms.TextInput(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+            'tags': forms.SelectMultiple(attrs={'class': 'form-control'})
+        }
+
+    def clean_slug(self):
+        new_slug = self.cleaned_data['slug'].lower()
+
+        if new_slug == 'create':
+            raise ValidationError('Slug may not be "create".')
+        return new_slug
